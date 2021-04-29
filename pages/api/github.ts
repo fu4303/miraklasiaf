3e52e313952139c8fd7f32fa07ad652a1ae37122
Acampbell <78562675+fu4303@users.noreply.github.com>
2021-04-29 14:12:07 -0400

    Update github.ts

--- pages/api/github.ts
+++ pages/api/github.ts
@@ -1,6 +1,6 @@
 export default async (_, res) => {
-  const userResponse = await fetch('https://api.github.com/users/miraklasiaf')
-  const userReposResponse = await fetch('https://api.github.com/users/miraklasiaf/repos')
+  const userResponse = await fetch('https://api.github.com/users/fu4303')
+  const userReposResponse = await fetch('https://api.github.com/users/fu4303/repos')
 
   const user = await userResponse.json()
   const repositories = await userReposResponse.json()
